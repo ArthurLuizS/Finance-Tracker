fe924f35491b94e6dc62efaf4e442a6f54ba065e
Arthur Luiz <arthurluiz36@gmail.com>
2024-08-26 18:14:06 -0300

    criado esqueleto do component 'Trend'

---
+++ components/trend.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+		<div class="font-bold" :class="[color]"> {{ title }}</div>
+
+		<div class="text-2xl font-extrabold text-black dark:text-white">
+			<USkeleton class="h-8 w-full" v-if="loading" />
+			<div v-else>{{ amount }}</div>
+		</div>
+		
+		<div>
+			<USkeleton class="h-8 w-full" v-if="loading" />
+			<div v-else class="flex space-x-1 items-center text-sm">
+				<UIcon :name="color === 'red' ? 'i-heroicons:arrow-trending-down' : 'i-heroicons:arrow-trending-up' " class="w-6 h-6" :class="[color]"/>
+				<div class="text-gray-500 dark:text-gray-400">
+					30% vs mês anterior
+				</div>
+			</div>
+
+		</div>
+
+  </div>
+</template>
+
+<script setup >
+defineProps({
+	title: String,
+	amount: Number,
+	lastAmount: Number,
+	color: String,
+	loading: Boolean
+})
+</script>
+
+<style>
+.green {
+	@apply text-green-700 dark:text-green-400
+}
+
+.red {
+	@apply text-red-700 dark:text-red-400
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
     <section class="flex items-center justify-between">
-        <h1 class="text-4xl fontSize-extrabold">Sumary</h1>
-
+        <h1 class="text-4xl font-extrabold">Sumary</h1>
         <div>
-            <USelect v-model="selectedView" :options="transactionViewOptions" />
+            <USelectMenu  v-model="selectedView" :options="transactionViewOptions" />
         </div>
+    </section>
+    <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+        <Trend title="inCome" :amount="400" :last-amount="3000" :loading="false" color="green" />
+        <Trend title="inCome" :amount="400" :last-amount="3000" :loading="false" color="red" />
+        <Trend title="inCome" :amount="400" :last-amount="3000" :loading="false" color="green" />
+        <Trend title="inCome" :amount="400" :last-amount="3000" :loading="false" color="red" />
     </section>
 </template>
 
 <script setup>
 import { transactionViewOptions } from '~/constants'
-const selectedView = [transactionViewOptions[0]]
+const selectedView = ref(transactionViewOptions[1])
 </script>
