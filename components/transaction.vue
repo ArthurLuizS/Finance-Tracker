01b674fe711fce8338a610381b9a91e5cbbd1a1c
Arthur Luiz <arthurluiz36@gmail.com>
2024-08-28 15:45:46 -0300

    Adicionado componente de Transaction

---
+++ components/transaction.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="grid grid-cols-2 py-4 border-b">
+		<div class="flex justify-between">
+			<div class="flex items-center space-x-1">
+				<UIcon name="i-heroicons-arrow-up-right" class="text-green-600"/>
+				<div>Salário</div>
+			</div>
+			<div>
+				<UBadge color="white">Categoria</UBadge>
+			</div>
+		</div>
+		<div class="flex justify-end space-x-2">
+			<div>{{ currency }}</div>
+			<div>
+				<UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+    			<UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
+  			</UDropdown>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const { currency } = useCurrency(300);
+const items = [
+[
+    {
+      label: 'Edit',
+      icon: 'i-heroicons-pencil-square-20-solid',
+      click: () => console.log('Edit')
+    },
+    {
+      label: 'Delete',
+      icon: 'i-heroicons-trash-20-solid',
+      click: () => console.log('Delete')
+    }
+  ]
+]
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,17 @@
         </div>
     </section>
     <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-        <Trend title="inCome" :amount="400" :last-amount="3000" :loading="false" color="green" />
+        <Trend title="inCome" :amount="0" :last-amount="3000" :loading="false" color="green" />
         <Trend title="inCome" :amount="400" :last-amount="3000" :loading="false" color="red" />
         <Trend title="inCome" :amount="400" :last-amount="3000" :loading="false" color="green" />
         <Trend title="inCome" :amount="400" :last-amount="3000" :loading="false" color="red" />
+    </section>
+
+    <section>
+        <Transaction />
+        <Transaction />
+        <Transaction />
+        <Transaction />
     </section>
 </template>
 
