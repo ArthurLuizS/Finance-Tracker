c0ff9d21ae01bd8641b30017b9c9e849891115c8
Arthur Luiz <arthurluiz36@gmail.com>
2024-08-28 17:21:58 -0300

    adicionado supabase

--- components/transaction.vue
+++ components/transaction.vue
@@ -13,7 +13,7 @@
 			<div>{{ currency }}</div>
 			<div>
 				<UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-    			<UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
+					<UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal" />
   			</UDropdown>
 			</div>
 		</div>
