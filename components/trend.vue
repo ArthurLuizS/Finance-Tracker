e34bc75d84e04b54cc8c90278b07e34aa87e8772
Arthur Luiz <arthurluiz36@gmail.com>
2024-08-28 15:44:59 -0300

    Adicionado composable para valores

--- components/trend.vue
+++ components/trend.vue
@@ -2,17 +2,17 @@
   <div>
 		<div class="font-bold" :class="[color]"> {{ title }}</div>
 
-		<div class="text-2xl font-extrabold text-black dark:text-white">
+		<div class="text-2xl font-extrabold text-black dark:text-white mb-2">
 			<USkeleton class="h-8 w-full" v-if="loading" />
-			<div v-else>{{ amount }}</div>
+			<div v-else>{{ currency }}</div>
 		</div>
 		
 		<div>
-			<USkeleton class="h-8 w-full" v-if="loading" />
+			<USkeleton class="h-6 w-full" v-if="loading" />
 			<div v-else class="flex space-x-1 items-center text-sm">
-				<UIcon :name="color === 'red' ? 'i-heroicons:arrow-trending-down' : 'i-heroicons:arrow-trending-up' " class="w-6 h-6" :class="[color]"/>
+				<UIcon :name="icon" class="w-6 h-6" :class="{'green': trendingUp, 'red': !trendingUp }"/>
 				<div class="text-gray-500 dark:text-gray-400">
-					30% vs mês anterior
+					{{ percentageTrend }} vs mês anterior
 				</div>
 			</div>
 
@@ -22,12 +22,28 @@
 </template>
 
 <script setup >
-defineProps({
+ const props = defineProps({
 	title: String,
 	amount: Number,
 	lastAmount: Number,
 	color: String,
 	loading: Boolean
+})
+
+const { currency } = useCurrency(props.amount)
+
+const trendingUp = computed(() => props.amount >= props.lastAmount)
+
+const icon = computed(() => trendingUp.value ? 'i-heroicons:arrow-trending-up' : 'i-heroicons:arrow-trending-down')
+
+const percentageTrend = computed(() =>{
+	if(props.amount === 0 || props.lastAmount === 0) return '♾️'
+	const bigger = Math.max(props.amount, props.lastAmount);
+	const lower = Math.min(props.amount, props.lastAmount);
+
+	const ratio = ((bigger - lower) / lower) * 100
+
+	return `${Math.ceil(ratio)}%`
 })
 </script>
 
