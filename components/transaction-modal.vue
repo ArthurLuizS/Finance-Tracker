d136e20c08a61c97a7ae7b974e415bf94f2b9cda
Arthur <arthurluiz36@gmail.com>
2024-09-13 13:01:18 -0300

    adicionado cetion de transação

---
+++ components/transaction-modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <UModal v-model="isOpen">
+    <UCard>
+      <template #header>
+        Adicionar Transação
+      </template>
+      oi
+    </UCard>
+  </UModal>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: Boolean,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const isOpen = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+})
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,37 @@
 <template>
-    <section class="flex items-center justify-between">
-        <h1 class="text-4xl font-extrabold">Sumary</h1>
-        <div>
-            <USelectMenu  v-model="selectedView" :options="transactionViewOptions" />
-        </div>
-    </section>
-    <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-        <Trend title="inCome" :amount="0" :last-amount="3000" :loading="loadingTransactions" color="green" />
-        <Trend title="inCome" :amount="400" :last-amount="3000" :loading="loadingTransactions" color="red" />
-        <Trend title="inCome" :amount="400" :last-amount="3000" :loading="loadingTransactions" color="green" />
-        <Trend title="inCome" :amount="400" :last-amount="3000" :loading="loadingTransactions" color="red" />
-    </section>
+	<section class="flex items-center justify-between">
+    <h1 class="text-4xl font-extrabold">Sumary</h1>
+    <div>
+      <USelectMenu  v-model="selectedView" :options="transactionViewOptions" />
+    </div>
+  </section>
 
-    <section>
-        <div v-for="(transactionOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10" v-if="!loadingTransactions">
-					<DayliTransactionSumary :transactions="transactionOnDay" :date="date" />
-					<Transaction v-for="transaction in transactionOnDay" :key="transaction" :transaction="transaction" @delete-transaction="onDeleteTransaction" :is-loading="deleteLoading"/>
-				</div>
-				<div v-else>
-					<USkeleton v-for="i in 4" :key="i" class="h-8 mb-2 w-full"/>
-				</div>
+  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+    <Trend title="Rendas" :amount="incomeTotal" :last-amount="3000" :loading="loadingTransactions" color="green" />
+    <Trend title="Despesas" :amount="expenseTotal" :last-amount="3000" :loading="loadingTransactions" color="red" />
+    <Trend title="Investimento" :amount="400" :last-amount="3000" :loading="loadingTransactions" color="green" />
+    <Trend title="inCome" :amount="400" :last-amount="3000" :loading="loadingTransactions" color="red" />
+  </section>
+
+	<section class="flex justify-between mb-10"> 
+		<div>
+			<h2 class="text-2xl font-extrabold">Transações</h2>
+			<div class="text-gray-500 dark:text-gray-400">Você tem {{ incomeCount }} registro de Renda e {{ expenseCount }} registro de Despesa nesse periodo</div>
+		</div>
+		<div>
+			<TransactionModal v-model="isOpen"/>
+			<UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" @click="isOpen = true" />
+		</div>
+	</section>
+
+  <section>
+    <div v-for="(transactionOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10" v-if="!loadingTransactions">
+			<DayliTransactionSumary :transactions="transactionOnDay" :date="date" />
+			<Transaction v-for="transaction in transactionOnDay" :key="transaction" :transaction="transaction" @delete-transaction="onDeleteTransaction" :is-loading="deleteLoading"/>
+		</div>
+			<div v-else>
+				<USkeleton v-for="i in 4" :key="i" class="h-8 mb-2 w-full"/>
+			</div>
     </section>
 </template>
 
@@ -33,6 +45,21 @@ const selectedView = ref(transactionViewOptions[1])
 const transactions = ref([])
 const loadingTransactions = ref(false)
 const deleteLoading = ref(false)
+const isOpen = ref(false)
+
+const income = computed(() => transactions.value.filter(t => t.type === 'Renda'))
+const expense = computed(() => transactions.value.filter(t => t.type === 'Despesa'))
+
+const incomeCount = computed(()=> income.value.length)
+const expenseCount = computed(()=> expense.value.length)
+
+const incomeTotal = computed(
+  () => income.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+)
+
+const expenseTotal = computed(
+  () => expense.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+)
 
 const fetchTransactions = async () => {
 	const { data } = await useAsyncData('transactions', async () => {
