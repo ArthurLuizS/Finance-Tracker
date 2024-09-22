16a21976675ac7f890cfd9dd84ed6d642dc8b713
Arthur Luiz <arthurluiz36@gmail.com>
2024-09-22 17:46:25 -0300

    composable para criação da data/mes/ano atual e seus respectivos anteriores

---
+++ composables/useSelectedTimePeriod.js
@@ -0,0 +1,50 @@
+import {
+  startOfYear,
+  endOfYear,
+  sub,
+  startOfDay,
+  endOfDay,
+  startOfMonth,
+  endOfMonth
+} from 'date-fns';
+export const useSelectedTimePeriod = (period) => {
+  const current = computed(() => {
+    switch (period.value) {
+      case 'Anual':
+        return {
+          from: startOfYear(new Date()),
+          to: new Date()
+        };
+      case 'Mensal':
+        return {
+          from: startOfMonth(new Date()),
+          to: new Date()
+        };
+      case 'Diário':
+        return {
+          from: startOfDay(new Date()),
+          to: new Date()
+        };
+    }
+  });
+  const previous = computed(() => {
+    switch (period.value) {
+      case 'Anual':
+        return {
+          from: startOfYear(sub(new Date(), { years: 1 })),
+          to: endOfYear(sub(new Date(), { years: 1 }))
+        };
+      case 'Mensal':
+        return {
+          from: startOfMonth(sub(new Date(), { months: 1 })),
+          to: endOfMonth(sub(new Date(), { months: 1 }))
+        };
+      case 'Diário':
+        return {
+          from: startOfDay(sub(new Date(), { days: 1 })),
+          to: endOfDay(sub(new Date(), { days: 1 }))
+        };
+    }
+  });
+  return { current, previous };
+};
