6a588cfffc268542299659264f42fd4b6d1c5174
Arthur Luiz <arthurluiz36@gmail.com>
2024-09-23 17:01:40 -0300

    criando experiencia de redirecionamento quando usuario é autenticado

---
+++ pages/confirm.vue
@@ -0,0 +1,9 @@
+<template>
+  <UCard>
+    <template #header> Redirecionando... </template>
+    Aguarde um momento enquanto o(a) redirecionamos...
+  </UCard>
+</template>
+<script setup>
+useRedirectIfAuthenticated();
+</script>
